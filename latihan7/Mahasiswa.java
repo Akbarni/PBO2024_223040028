cf52520f2d2b86c87c68a5421d07801672f0b13b
Akbar Nur iman <122083444+Akbarni@users.noreply.github.com>
2024-03-28 23:29:48 +0700

    Add files via upload

---
+++ latihan7/Mahasiswa.java
@@ -0,0 +1,40 @@
+package latihan7;
+
+/**
+ * 
+ * @author AKBAR NUR IMAN
+ *
+ */
+public class Mahasiswa {
+    private String NRP;
+    private String nama;
+    
+    public Mahasiswa(String nrp, String nama){
+        this.NRP = nrp;
+        this.nama = nama;
+    }
+    
+    public String display(){
+     return "NRP: " + NRP + " Nama : " + nama; 
+    }
+    
+    
+//    SETTER DAN GETTER
+
+    public String getNRP() {
+        return NRP;
+    }
+
+    public void setNRP(String NRP) {
+        this.NRP = NRP;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+}
---
+++ latihan7/TranskripNilaiMain.java
@@ -0,0 +1,31 @@
+package latihan7;
+
+/**
+ * 
+ * @author AKBAR NUR IMAN
+ *
+ */
+public class TranskripNilaiMain {
+    public static void main(String[] args) {
+    //    OBJEK MATAKULIAH
+        MataKuliah mk1 = new MataKuliah("001", "Algoritma Pemograman 1","A",3);
+        MataKuliah mk2 = new MataKuliah("002", "Algoritma Pemograman 2","BC",3);
+        MataKuliah mk3 = new MataKuliah("003", "Pemograman Berorientasi Objek","B",3);
+        
+    //     OBJEK KHS
+        KartuHasilStudi khs = new KartuHasilStudi("20222");
+        khs.addMataKuliah(mk1);
+        khs.addMataKuliah(mk2);
+        khs.addMataKuliah(mk3);
+
+    //     OBJEK MAHASISWA
+        Mahasiswa mhs = new Mahasiswa("223040028", "Akbar.N.I");
+
+    //     OBJEK TRANSKRIPT
+        TranskripNilai transkrip = new TranskripNilai(mhs);
+        transkrip.addKHS(khs);
+        transkrip.hitungIPK();
+        transkrip.display();
+    
+    }
+}
